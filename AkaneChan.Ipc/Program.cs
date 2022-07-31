c036e11015814391169f4cdcad3c70e578ed00ea
deflis <deflis@gmail.com>
2022-08-01 04:50:58 +0900

    AIVOICEの偽装クライアントを実装する

---
+++ AkaneChan.Ipc/Program.cs
@@ -0,0 +1,41 @@
+﻿using ZundaChan.Core;
+using ZundaChan.Core.BouyomiIpc;
+using ZundaChan.Core.Aivoice;
+
+{
+    var config = new NLog.Config.LoggingConfiguration();
+
+    // Targets where to log to: File and Console
+    var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+    // Rules for mapping loggers to targets            
+    config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logconsole);
+    // Apply config           
+    NLog.LogManager.Configuration = config;
+}
+var logger = NLog.LogManager.GetCurrentClassLogger();
+
+
+var proxy = new AivoiceProxy();
+try
+{
+    var ipcServer = new IpcServer(proxy);
+    logger.Info("IPCサーバを起動しました");
+}
+catch (Exception ex)
+{
+    logger.Fatal("IPCサーバの起動に失敗しました", ex);
+    return 1;
+}
+
+while (true)
+{
+    var key = Console.ReadKey();
+    if (key.KeyChar == 'q' || key.Key == ConsoleKey.Escape)
+    {
+        return 0;
+    }
+    else if (key.KeyChar == 'r')
+    {
+        Config.Reload();
+    }
+}
---
+++ ZundaChan.Core.Aivoice/AivoiceProxy.cs
@@ -0,0 +1,85 @@
+﻿using AI.Talk.Editor.Api;
+using System.Collections.Concurrent;
+
+namespace ZundaChan.Core.Aivoice
+{
+    public class AivoiceProxy : IProxy, IDisposable
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private TtsControl ttsControl;
+        private BlockingCollection<string> playTalkJobs = new BlockingCollection<string>();
+
+        public AivoiceProxy()
+        {
+            var thread = new Thread(new ThreadStart(OnStart));
+            thread.IsBackground = true;
+            thread.Start();
+
+            ttsControl = new TtsControl();
+            var hostnames= ttsControl.GetAvailableHostNames();
+            ttsControl.Initialize(hostnames[0]);
+
+            if (ttsControl.Status == HostStatus.NotRunning)
+            {
+                // ホストプログラムを起動する
+                ttsControl.StartHost();
+            }
+
+            // ホストプログラムに接続する
+            ttsControl.Connect();
+
+            Logger.Info("ホストバージョン: " + ttsControl.Version);
+            Logger.Info("ホストへの接続を開始しました。");
+
+        }
+
+        public void Dispose()
+        {
+            ttsControl.Disconnect();
+        }
+
+        public int AddTalkTask(string text)
+        {
+            TalkTask(text);
+            return 0;
+        }
+
+        private void TalkTask(string text)
+        {
+            try
+            {
+                playTalkJobs.Add(text);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("ボイス生成でエラーが発生しました", ex);
+            }
+        }
+
+        private async void OnStart()
+        {
+            foreach (var text in playTalkJobs.GetConsumingEnumerable(CancellationToken.None))
+            {
+                try
+                {
+                    await PlayVoiceAsync(text);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("ボイス再生でエラーが発生しました", ex);
+                }
+            }
+        }
+
+        private async Task PlayVoiceAsync(string text)
+        {
+            ttsControl.Text = text;
+            ttsControl.Play();
+
+            while (ttsControl.Status == HostStatus.Busy)
+            {
+                await Task.Delay(100);
+            }
+        }
+    }
+}
